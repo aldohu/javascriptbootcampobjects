504614a2e2d89e850a7c19e8239aafa7ab385034
Aldin <alhuric89@gmail.com>
2021-10-30 02:04:44 +0200

    add objectsFuncitons file

--- object101.js
+++ object101.js
@@ -1,1 +1,27 @@
-let simpleObj = {key: "value"};
+let myBook = {
+    author: "George Orwell",
+    title: "1984",
+    pageCount: 326
+};
+
+
+console.log(`${myBook.title} by ${myBook.author}`);
+
+
+myBook.title = "Animal Farm";
+console.log(`${myBook.title} by ${myBook.author}`);
+
+
+//Challenge area
+
+let person = {
+    name: "Andrew",
+    age: 27,
+    location: "Philadelphia"
+}
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}.`);
+
+person.age = ++person.age;
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}.`); 
